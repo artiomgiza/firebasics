3d7dfc192b0a3530161eff152041753000321903
Artiom Giza <artiomg@gettaxi.com>
2018-06-16 13:20:21 +0300

    store image, get URL to stored omg

--- public/app.js
+++ public/app.js
@@ -1,34 +1,32 @@
   document.addEventListener("DOMContentLoaded", function(event) {
     const app = firebase.app();
     const db = firebase.firestore();
+    const settings = {/* your settings... */ timestampsInSnapshots: true};
+    db.settings(settings);
     const myPost = db.collection('posts').doc('firstpost');
 
 
-    const productsRef = db.collection('products');
-    const query = productsRef.where('price', '>', 10).orderBy('price', 'desc').limit(1)
-    query.get()
-    .then(products => {
-        products.forEach(product => {
-            data = product.data();
-            document.write(`${data.name} at ${data.price} <br>`)
-        })
-    })
 
 
-    // here we can do simple .get().then(...) -> this allows us to get the data
-    // without listening. onSnapshot will listen to the data changes...
-
-    // myPost.onSnapshot(doc => {
-    //     // onSnapshot returns the stream, each time doc is changed - it will be passed to this code:
-    //     const data = doc.data();
-    //     document.getElementById("title").innerHTML = data.title 
-    // })
   });
 
-function updatePost(e) {
-    const db = firebase.firestore();
-    const myPost = db.collection('posts').doc('firstpost');
-    myPost.update( { title: e.target.value })
+function updateFile(files) {
+    const storageRef = firebase.storage().ref();
+    const horseRef = storageRef.child('horse.jpg');
+   
+    const file = files.item(0);
+    const task = horseRef.put(file);
+
+    task.then(snapshot => {
+        console.log(snapshot);
+        // const url = snapshot.downloadURL      
+
+        horseRef.getDownloadURL().then(function(url) {
+            console.log(url);
+            document.getElementById('imgUpload').setAttribute('src',url)
+          });
+
+    })
 }
 
 function googleLogin() {
